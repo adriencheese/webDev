3e146c13078bdda386440862ab89905abbacc888
adriencheese <adrien.qi304@gmail.com>
2023-02-22 17:25:35 -0500

    default

--- spotify-sandbox/web-api-auth-examples-master/authorization_code/app.js
+++ spotify-sandbox/web-api-auth-examples-master/authorization_code/app.js
@@ -13,9 +13,9 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+var client_id = '8ea83afbcd234a20ab2130ff58dd457a'; // Your client id
+var client_secret = 'bcf75214802644ab90c77258173a0c34'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
